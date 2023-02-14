2f2503b1647df305be33c38e77ab0a3009962d33
joaopaulom-dev <joaopaulomdev.t2@gmail.com>
2023-02-14 14:35:16 -0300

    Apresentando texto na tela

---
+++ java/src/main/java/joaoribeiro/java/Java.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package joaoribeiro.java;
+
+/**
+ *
+ * @author joao.paulo
+ */
+public class Java {
+
+    public static void main(String[] args) {
+        //Instancia novo objeto da classe
+        OlaMundo instancia = new OlaMundo();
+        
+        //Apresenta mensagem na tela
+        instancia.olaMundo();
+    }
+    
+}
---
+++ java/src/main/java/joaoribeiro/java/OlaMundo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package joaoribeiro.java;
+
+/**
+ *
+ * @author joao.paulo
+ */
+public class OlaMundo {
+    
+    public void olaMundo(){
+        //Apresenta mensagem na tela
+        System.out.println("Ola mundo!");
+    }
+    
+}
